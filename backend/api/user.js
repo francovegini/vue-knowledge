e1583712475fae2615c010046da69c7322316069
francovegini <francovegini@hotmail.com>
2021-01-12 21:53:02 -0300

    Implementado get - users

--- backend/api/user.js
+++ backend/api/user.js
@@ -47,5 +47,12 @@ module.exports = app => {
         }
     }
 
+    const get = (req, res) => {
+        app.db('users')
+            .select('id', 'name', 'email', 'admin')
+            .then(users => res.json(users))
+            .catch(err => res.status(500).send(err));
+    }
+
     return { save };
 }
