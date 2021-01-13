51f1f227a3016e5a2eb6150e2370c96ad0dd4677
francovegini <francovegini@hotmail.com>
2021-01-12 21:47:14 -0300

    Iniciado implementação api de usuarios

--- backend/api/user.js
+++ backend/api/user.js
@@ -1,6 +1,34 @@
+const bcrypt = require('bcrypt-nodejs');
+
 module.exports = app => {
-    const save = (req, res) => {
-        res.send('User save');
+    const { existsOrError, notExistsOrError, equalsOrError } = app.api.validation;
+
+    const encryptPassword = password => {
+        const salt = bcrypt.genSaltSync(10);
+        return bcrypt.hashSync(password, salt);
+    }
+
+    const save = async (req, res) => {
+        const user = { ...req.body };
+        if (req.params.id) {
+            user.id = req.params.id;
+        }
+
+        try {
+            existsOrError(user.name, 'Nome não informado');
+            existsOrError(user.email, 'E-mail não informado');
+            existsOrError(user.password, 'Senha não informada');
+            existsOrError(user.confirmPassword, 'Confirmação de senha inválida');
+            equalsOrError(user.password, user.confirmPassword, 'Senhas não conferem');
+
+            const userFromDB = await app.db('users')
+                .where({ email: user.email }).first();
+            if (!user.id) {
+                notExistsOrError(userFromDB, "Usuário já foi cadastrado")
+            }
+        } catch (msg) {
+            return res.status(400).send(msg);
+        }
     }
 
     return { save };
