c92aa7bc7b4c1a4cfa6057b41fa2fa595bd1af2d
francovegini <francovegini@hotmail.com>
2021-01-14 22:49:07 -0300

    get que traz o path junto

--- backend/api/category.js
+++ backend/api/category.js
@@ -48,4 +48,31 @@ module.exports = app => {
             res.status(400).send(msg);
         }
     }
+
+    const withPath = categories => {
+        const getParent = (categories, parentId) => {
+            const parent = categories.filter(parent => parent.id === parentId);
+            return parent.length ? parent[0] : null;
+        }
+
+        const categoriesWithPath = categories.map(category => {
+            let path = category.name;
+            let parent = getParent(categories, category.parentId);
+
+            while (parent) {
+                path = `${ parent.name } > ${ path }`;
+                parent = getParent(categories, parent.parentId)
+            }
+
+            return { ...category, path };
+        });
+
+        categoriesWithPath.sort((a, b) => {
+            if(a.path < b.path) return -1;
+            if(a.path > b.path) return 1;
+            return 0;
+        });
+
+        return categoriesWithPath;
+    }
 }
