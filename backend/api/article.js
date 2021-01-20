6063e7975266c366197a6f25269acb2484ff91fa
francovegini <francovegini@hotmail.com>
2021-01-19 22:10:04 -0300

    Implementado get e getById

--- backend/api/article.js
+++ backend/api/article.js
@@ -34,16 +34,41 @@ module.exports = app => {
     }
 
     const remove = async (req, res) => {
-
         try {
             const rowsDeleted = await app.db('articles')
                 .where({ id: req.params.id }).del();
             notExistsOrError(rowsDeleted, "Artigo não foi encontrado.");
-        } catch (msg) {
-            res.status(400).send(msg);
-        }
 
+            res.status(204).send();
+        } catch (msg) {
+            res.status(500).send(msg);
+        }
     }
 
-    return { save, remove };
+    const limit = 10;
+
+    const get = async (req, res) => {
+        const page = req.query.page || 1;
+
+        const result = await app.db('articles').count('id').first();
+        const count = parseInt(result.count);
+
+        app.db('articles')
+            .select('id', 'name', 'description')
+            .limit(limit).offset(page * limit - limit)
+            .then(articles => res.json({ data: articles, count, limit }))
+            .catch(err => res.status(500).send(err));
+    }
+
+    const getById = (req, res) => {
+        app.db('articles')
+            .where({ id: req.params.id }).first()
+            .then(article => {
+                article.content = article.content.toString();
+                return res.json(article);
+            })
+            .catch(err => res.status(500).send(err));
+    }
+
+    return { save, remove, get, getById };
 }
