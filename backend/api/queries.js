9d66e47270a72a110d087138d6ae479cd64e98b7
francovegini <francovegini@hotmail.com>
2021-01-25 23:39:47 -0300

    Implementado categoryWithChildren - query

--- backend/api/queries.js
+++ backend/api/queries.js
@@ -1,5 +1,9 @@
 module.exports = {
     categoryWithChildren: `
-    
+        WITH RECURSIVE subcategories (id) AS (
+        SELECT id FROM categories WHERE id = ?
+        UNION ALL 
+        SELECT c.id FROM subcategories, categories c 
+            WHERE "parentId" = subcategories.id
     `
 }
