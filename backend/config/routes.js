03dc0cfa0dee7a18b8175d71299358354f9cb5ae
francovegini <francovegini@hotmail.com>
2021-01-25 23:44:09 -0300

    Route

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -28,4 +28,7 @@ module.exports = app => {
         .put(app.api.article.save)
         .delete(app.api.article.remove);
 
+    app.route('/categories/:id/articles')
+        .get(app.api.article.getByCategory);
+
 }
