df6af7ab5ba62d2c08c85ba4b77535f7c7a229aa
francovegini <francovegini@hotmail.com>
2021-01-25 23:42:37 -0300

    Ajuste query

--- backend/api/queries.js
+++ backend/api/queries.js
@@ -1,9 +1,16 @@
 module.exports = {
     categoryWithChildren: `
         WITH RECURSIVE subcategories (id) AS (
-        SELECT id FROM categories WHERE id = ?
-        UNION ALL 
-        SELECT c.id FROM subcategories, categories c 
+            SELECT id
+            FROM categories
+            WHERE id = ?
+            UNION ALL
+            SELECT c.id
+            FROM subcategories,
+                 categories c
             WHERE "parentId" = subcategories.id
+        )
+        SELECT id
+        from subcategories
     `
 }
