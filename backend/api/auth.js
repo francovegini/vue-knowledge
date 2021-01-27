2dcd23f7d7f72935e8f5404513b0b4e5cf6d9c7d
francovegini <francovegini@hotmail.com>
2021-01-26 23:27:56 -0300

    Finalizado auth.js

--- backend/api/auth.js
+++ backend/api/auth.js
@@ -38,4 +38,23 @@ module.exports = app => {
         })
     }
 
+    const validateToken = async (req, res) => {
+        const userData = req.body || null;
+
+        try {
+            if (userData) {
+                const token = jwt.decode(userData.token, authSecret);
+                if (new Date(token.exp * 1000) > new Date()) {
+                    return res.send(true);
+                }
+            }
+        } catch {
+            // Problema com o token
+        }
+
+        res.send(false);
+    }
+
+    return { signin, validateToken }
+
 }
