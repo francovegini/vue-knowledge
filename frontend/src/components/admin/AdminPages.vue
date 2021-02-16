db322f6062e159807f021543375d241907d45f2a
francovegini <francovegini@hotmail.com>
2021-02-15 23:33:40 -0300

    Criado componente de Admin

---
+++ frontend/src/components/admin/AdminPages.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="admin-pages">
+        <PageTitle icon="fa fa-cogs" main="Administração do sistema" sub="Cadastro & Cia"/>
+    </div>
+</template>
+
+<script>
+import PageTitle from "@/components/template/PageTitle";
+
+export default {
+    name: "AdminPages",
+    components: { PageTitle }
+}
+</script>
+
+<styles>
+
+</styles>
--- frontend/src/components/home/Home.vue
+++ frontend/src/components/home/Home.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="home">
         <PageTitle icon="fa fa-home" main="Dashboard" sub="Base de conhecimento"/>
     </div>
 </template>
