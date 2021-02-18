95e8c1a0b77c21c1764aa6c93fdaddf8804a325f
francovegini <francovegini@hotmail.com>
2021-02-17 23:10:21 -0300

    Componente Stat

---
+++ frontend/src/components/home/Stat.vue
@@ -0,0 +1,19 @@
+<template>
+
+</template>
+
+<script>
+export default {
+    name: "Stat",
+    props: ['title', 'value', 'icon', 'color'],
+    computed: {
+        style() {
+            return "color: " + (this.color || "#000");
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
