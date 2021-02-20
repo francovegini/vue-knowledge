2955caf0deedb6b2d0ca6c185bb8f9b73d1be71d
francovegini <francovegini@hotmail.com>
2021-02-19 23:37:19 -0300

    Criado carcaça inicial - Tela de admin

--- frontend/src/components/admin/AdminPages.vue
+++ frontend/src/components/admin/AdminPages.vue
@@ -1,6 +1,21 @@
 <template>
     <div class="admin-pages">
         <PageTitle icon="fa fa-cogs" main=" Administração do sistema" sub="Cadastro & Cia"/>
+        <div class="admin-pages-tabs">
+            <b-card no-body>
+                <b-tabs card>
+                    <b-tab title="Artigos" active>
+                        <h1>Artigo</h1>
+                    </b-tab>
+                    <b-tab title="Categorias">
+                        <h1>Categorias</h1>
+                    </b-tab>
+                    <b-tab title="Usuários">
+                        <h1>Usuários</h1>
+                    </b-tab>
+                </b-tabs>
+            </b-card>
+        </div>
     </div>
 </template>
 
@@ -13,6 +28,6 @@ export default {
 }
 </script>
 
-<styles>
+<style>
 
-</styles>
+</style>
