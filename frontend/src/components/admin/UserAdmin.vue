2f582a7579571020d216d17da39f04b68a2591d2
francovegini <francovegini@hotmail.com>
2021-02-21 00:18:01 -0300

    Iniciado implementacao UserAdmin

--- frontend/src/components/admin/UserAdmin.vue
+++ frontend/src/components/admin/UserAdmin.vue
@@ -5,8 +5,30 @@
 </template>
 
 <script>
+import { baseApiUrl } from "@/global";
+import axios from "axios";
+
 export default {
-    name: "UserAdmin"
+    name: "UserAdmin",
+    data: function () {
+        return {
+            mode: 'save',
+            user: {},
+            users: []
+        }
+    },
+    methods: {
+        loadUsers() {
+            const url = `${ baseApiUrl }/users`
+            axios.get(url).then(res => {
+                this.users = res.data;
+                console.log(this.users);
+            })
+        }
+    },
+    mounted() {
+        this.loadUsers();
+    }
 }
 </script>
 
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -9,7 +9,7 @@ import router from "./config/router";
 Vue.config.productionTip = false
 
 // TEMPORÁRIO - Utilizado para fazer requisição sem estar autenticado
-require('axios').defaults.headers.common['Authorization'] = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MiwibmFtZSI6IkZyYW5jbyBWZWdpbmkiLCJlbWFpbCI6ImZyYW5jb3ZlZ2luaUBob3RtYWlsLmNvbSIsImFkbWluIjp0cnVlLCJpYXQiOjE2MTM2MTQ2OTcsImV4cCI6MTYxMzg3Mzg5N30.Pvkn7ezFY2uZ2lpC9A-41nzCN5QKFzlvqXkvRUfEW5g";
+require('axios').defaults.headers.common['Authorization'] = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MiwibmFtZSI6IkZyYW5jbyBWZWdpbmkiLCJlbWFpbCI6ImZyYW5jb3ZlZ2luaUBob3RtYWlsLmNvbSIsImFkbWluIjp0cnVlLCJpYXQiOjE2MTQxMzYyMjAsImV4cCI6MTYxNDM5NTQyMH0.N0eBPtC_Y_CcxjRgJl_5fW_kbkY7YtQJ_REsl2slReA";
 
 new Vue({
     store,
