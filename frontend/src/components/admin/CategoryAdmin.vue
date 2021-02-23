fbae9231634e09abfc3735a9031c4eaae573e4cb
francovegini <francovegini@hotmail.com>
2021-02-23 16:27:38 -0300

    Iniciado implementação componente categories

--- frontend/src/components/admin/CategoryAdmin.vue
+++ frontend/src/components/admin/CategoryAdmin.vue
@@ -1,12 +1,27 @@
 <template>
     <div class="category-admin">
-        <h1>Categoria Componente</h1>
+        <b-table hover striped :items="categories" :fields="fields">
+        </b-table>
     </div>
 </template>
 
 <script>
 export default {
-    name: "CategoryAdmin"
+    name: "CategoryAdmin",
+    data: function () {
+        return {
+            mode: 'save',
+            category: {},
+            categories: [],
+            fields: [
+                { key: 'id', label: 'Código', sortable: true },
+                { key: 'name', label: 'Nome', sortable: true },
+                { key: 'path', label: 'Caminho', sortable: true },
+                { key: 'actions', label: 'Ações' }
+            ]
+        }
+    },
+    methods: {}
 }
 </script>
 
