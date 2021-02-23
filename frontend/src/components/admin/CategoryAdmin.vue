ea7439fdf5d33e805f6ffb68e6e358de443cfa7a
francovegini <francovegini@hotmail.com>
2021-02-23 16:43:47 -0300

    formulario do componente de categorias

--- frontend/src/components/admin/CategoryAdmin.vue
+++ frontend/src/components/admin/CategoryAdmin.vue
@@ -1,5 +1,22 @@
 <template>
     <div class="category-admin">
+        <b-form>
+            <input id="category-id" type="hidden" v-model="category.id"/>
+            <b-row>
+                <b-col sm="12" md="12">
+                    <b-form-group label="Nome:" label-for="category-name">
+                        <b-form-input id="category-name" type="text" v-model="category.name" required
+                                      placeholder="Informe o nome da categoria" :readonly="mode === 'remove'"/>
+                    </b-form-group>
+                </b-col>
+                <b-col sm="12" md="12">
+                    <b-form-group label="Categoria pai:" label-for="category-parentId">
+                        <b-form-select id="category-parentId" :options="categories" v-model="category.parentId"/>
+                    </b-form-group>
+                </b-col>
+            </b-row>
+        </b-form>
+
         <b-table hover striped :items="categories" :fields="fields">
         </b-table>
     </div>
@@ -28,7 +45,9 @@ export default {
         loadCategories() {
             axios.get(`${ baseApiUrl }/categories`)
                 .then(res => {
-                    this.categories = res.data;
+                    this.categories = res.data.map(category => {
+                        return { ...category, value: category.id, text: category.path }
+                    })
                 })
         }
     },
