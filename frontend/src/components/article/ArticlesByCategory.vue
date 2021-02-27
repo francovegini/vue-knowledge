757e3c7b2ece7f86bd653113f6d1de30c1d9eff7
francovegini <francovegini@hotmail.com>
2021-02-27 14:25:08 -0300

    Iniciado implementacao do componente ArticlesByCategory

---
+++ frontend/src/components/article/ArticlesByCategory.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="articles-by-category">
+        <PageTitle icon="fa fa-folder-o" :main="category.name" sub="Categoria"/>
+    </div>
+
+</template>
+
+<script>
+import { baseApiUrl } from "@/global";
+import axios from "axios";
+import PageTitle from "@/components/template/PageTitle";
+
+export default {
+    name: "ArticlesByCategory",
+    components: { PageTitle },
+    data: function () {
+        return {
+            category: {},
+            articles: [],
+            page: 1,
+            loadMore: true
+        }
+    },
+    methods: {
+        getCategory() {
+            const url = `${ baseApiUrl }/categories/${ this.category.id }`;
+            axios.get(url)
+                .then(res => this.category = res.data);
+        }
+    }
+}
+</script>
+
+<style>
+</style>
