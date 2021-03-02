efbc4f48e3e81a9d167a15f9a1b391b7527cf883
francovegini <francovegini@hotmail.com>
2021-03-01 23:06:15 -0300

    Componente ArticleItem

---
+++ frontend/src/components/article/ArticleItem.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="article-item">
+        <router-link :to="{name: 'articleById', params: { id: article.id} }">
+            <div class="article-item-image d-none d-sm-block">
+                <img v-if="article.imageUrl" :src="article.imageUrl" height="150" width="150" alt="Article"/>
+                <img v-else src="@/assets/article.png" height="150" width="150"/>
+            </div>
+            <div class="article-item-info">
+                <h2>{{ article.name }}</h2>
+                <p>{{ article.description }}</p>
+                <span class="article-item-author">
+                    <strong>Autor: </strong> {{ article.author }}
+                </span>
+            </div>
+        </router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ArticleItem",
+    props: ['article']
+}
+</script>
+
+<style>
+
+</style>
