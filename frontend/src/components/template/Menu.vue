a2e3b9fb99054ee6286118e8f5ab5a94af2a2885
francovegini <francovegini@hotmail.com>
2021-03-02 18:32:25 -0300

    Adicionado menu no filtro

--- frontend/src/components/template/Menu.vue
+++ frontend/src/components/template/Menu.vue
@@ -1,6 +1,10 @@
 <template>
     <aside class="menu" v-show="isMenuVisible">
-        <Tree :data="treeData" :options="treeOptions" ref="tree"/>
+        <div class="menu-filter">
+            <i class="fa fa-search fa-lg"></i>
+            <input type="text" placeholder="Digite para filtrar" v-model="treeFilter" class="filter-field">
+        </div>
+        <Tree :data="treeData" :options="treeOptions" :filter="treeFilter" ref="tree"/>
     </aside>
 </template>
 
@@ -16,9 +20,11 @@ export default {
     computed: mapState(['isMenuVisible']),
     data: function () {
         return {
+            treeFilter: '',
             treeData: this.getTreeData(),
             treeOptions: {
-                propertyNames: { 'text': 'name' }
+                propertyNames: { 'text': 'name' },
+                filter: { emptyText: 'Categoria não encontrada' }
             }
         }
     },
@@ -55,6 +61,30 @@ export default {
 }
 
 .tree-arrow.has-child {
-    filter: brightness(2);
+	filter: brightness(2);
+}
+
+.menu .menu-filter {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+
+	margin: 20px;
+	padding-bottom: 8px;
+	border-bottom: 1px solid #AAA;
+}
+
+.menu .menu-filter i {
+	color: #AAA;
+	margin-right: 10px;
+}
+
+.menu input {
+	color: #CCC;
+	font-size: 1.3rem;
+	border: 0;
+	outline: 0;
+	width: 100%;
+	background: transparent;
 }
 </style>
