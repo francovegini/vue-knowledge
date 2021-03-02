5749bb3d969734ce003b5cea0c880f18bc33b68a
francovegini <francovegini@hotmail.com>
2021-03-01 22:17:14 -0300

    Atualizado token + removido row e col desnecessarias

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -10,7 +10,7 @@ import router from "./config/router";
 Vue.config.productionTip = false
 
 // TEMPORÁRIO - Utilizado para fazer requisição sem estar autenticado
-require('axios').defaults.headers.common['Authorization'] = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MiwibmFtZSI6IkZyYW5jbyBWZWdpbmkiLCJlbWFpbCI6ImZyYW5jb3ZlZ2luaUBob3RtYWlsLmNvbSIsImFkbWluIjp0cnVlLCJpYXQiOjE2MTQzMDg0ODEsImV4cCI6MTYxNDU2NzY4MX0.h9Mpkjfrk_uFWaDFDtgNNnUOBunPUZwt5bzsFvwjzbI";
+require('axios').defaults.headers.common['Authorization'] = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MiwibmFtZSI6IkZyYW5jbyBWZWdpbmkiLCJlbWFpbCI6ImZyYW5jb3ZlZ2luaUBob3RtYWlsLmNvbSIsImFkbWluIjp0cnVlLCJpYXQiOjE2MTQ2NDc3MDksImV4cCI6MTYxNDkwNjkwOX0.RlSPaqfzwyuOd3EmKw9hPc2odk1lhz-5jMBXUf9Pijc";
 
 new Vue({
     store,
