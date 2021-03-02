a159a9532a28690c3f99fee5857adc688fef7ec8
francovegini <francovegini@hotmail.com>
2021-03-01 22:26:05 -0300

    Estrutura inicial do componente para visualizacao dos artigos

---
+++ frontend/src/components/article/ArticleById.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="article-by-id">
+        <PageTitle icon="fa fa-file-o" :main="article.name" :sub="article.description"/>
+        <div class="article-content" v-html="article.content"></div>
+    </div>
+</template>
+
+<script>
+import { baseApiUrl } from "@/global";
+import axios from "axios";
+import PageTitle from "@/components/template/PageTitle";
+
+export default {
+    name: "ArticleById",
+    components: { PageTitle },
+    data: function () {
+        return {
+            article: {}
+        }
+    },
+    mounted() {
+        const url = `${ baseApiUrl }/articles/${ this.$route.params.id }`
+
+        axios.get(url)
+            .then(res => this.article = res.data);
+    }
+}
+</script>
+
+<style>
+
+</style>
