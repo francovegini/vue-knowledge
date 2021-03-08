ebfecad32bf566972814cd4757ef53cd30bbcdc5
francovegini <francovegini@hotmail.com>
2021-03-08 00:27:33 -0300

    Template login

--- frontend/src/components/auth/Auth.vue
+++ frontend/src/components/auth/Auth.vue
@@ -2,6 +2,21 @@
     <div class="auth-content">
         <div class="auth-modal">
             <img src="@/assets/logo.png" width="200" alt="Logo"/>
+            <hr>
+            <div class="auth-title">{{ showSignup ? 'Cadastro' : 'Login' }}</div>
+
+            <input v-if="showSignup" v-model="user.name" type="text" placeholder="Nome">
+            <input v-model="user.email" type="text" placeholder="E-mail">
+            <input v-model="user.password" type="password" placeholder="Senha">
+            <input v-if="showSignup" v-model="user.password" type="password" placeholder="Confirme a senha">
+
+            <button v-if="showSignup" @click="signup">Registrar</button>
+            <button v-else @click="signin">Entrar</button>
+
+            <a href @click.prevent="showSignup = !showSignup">
+                <span v-if="showSignup">Já tem cadastro? Acesse o Login!</span>
+                <span v-else>Não tem cadastro? Registre-se aqui!</span>
+            </a>
         </div>
     </div>
 </template>
